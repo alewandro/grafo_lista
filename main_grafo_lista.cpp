04d0d6425dd78e0ceb0242d5e0588bbdf4224dc6
Unknown <alewandro.dev@gmail.com>
2017-09-19 22:08:56 -0300

    orden topologico y clasificacion de arcos

--- main_grafo_lista.cpp
+++ main_grafo_lista.cpp
@@ -3,10 +3,18 @@
 #include "Grafo.h"
 #include "list"
 #include "vector"
+#include "stack"
 
 using namespace std;
 
+struct datos_dearcos{           //clasifica los arcos
+    int origen;
+    int destino;
+    char tipo;
+};
+
 void imprimir_lista_arcos(list<Arco> una_lista){
+
     if (!una_lista.empty()){
     	cout << " D  C" << endl;
     	list<Arco>::iterator iterador= una_lista.begin();
@@ -21,7 +29,7 @@ void imprimir_lista_arcos(list<Arco> una_lista){
 
 void imprimir_lista_vertices(list<int> una_lista){
     if (!una_lista.empty()){
-        cout << endl;
+        //cout << endl;
         list<int>::iterator iterador= una_lista.begin();
         while (iterador != una_lista.end()) {
             cout << *iterador << "  ";
@@ -29,6 +37,68 @@ void imprimir_lista_vertices(list<int> una_lista){
         }
         cout << endl;
     }
+}
+
+void imprimir_grafo(Grafo &un_grafo){
+    cout << endl << endl;
+
+    list<int> lista_vertices;
+    un_grafo.devolver_vertices(lista_vertices);
+
+    int longitud= un_grafo.devolver_longitud();
+    list<Arco> una_lista_arcos;
+    
+    for (int i=0; i< longitud; i++){
+        cout << endl;
+        un_grafo.devolver_adyacentes(i, una_lista_arcos);
+        list<Arco>::iterator iterador= una_lista_arcos.begin();
+        while (iterador != una_lista_arcos.end()) {
+            Arco arko= *iterador;
+            int destino= arko.Devolver_destino();
+
+            if (destino<i){
+            
+                // se posiciona en el vertice destino
+                for(int j=1; j<destino*3; j++)
+                    cout << " ";
+                cout << " ";
+                for(int k=destino*3; k<=i*3; k++)
+                    cout << "«";            
+            }else if (destino == i){
+                // se posiciona en el vertice destino
+                for(int j=1; j<destino*3; j++)
+                    cout << " ";
+                cout << "↓" << endl;
+            }
+            iterador++;
+        }    
+    }    
+
+    imprimir_lista_vertices(lista_vertices);
+
+    for (int i=0; i< longitud; i++){
+        un_grafo.devolver_adyacentes(i, una_lista_arcos);
+        list<Arco>::iterator iterador= una_lista_arcos.begin();
+        while (iterador != una_lista_arcos.end()) {
+            Arco arko= *iterador;
+            int destino= arko.Devolver_destino();
+
+            if (destino>i){
+                // se posiciona en el vertice origen
+                cout << " ";
+                for(int j=1; j<i*3; j++)
+                    cout << " ";
+            
+                // imprime un arco y va al renglon siguiente
+                for(int k=i*3; k<destino*3; k++)
+                    cout << "»";
+                cout << " " << endl;                
+
+            }
+            iterador++;
+        }        
+    }
+
 }
 
 void imprimir_menu(){
@@ -43,18 +113,19 @@ void imprimir_menu(){
     cout << "| 6- ver Arcos Entrantes        | 13- Listar Vertices del Grafo         |" << endl;
     cout << "| 7- ver Arcos Adyacentes       |                                       |" << endl;
     cout << "------------------------------------------------------------------------o" << endl;
-    cout << "| 14- DFS                                                               |" << endl;
-    cout << "| 15- BFS                                                               |" << endl;
+    cout << "| 14- Imprimir el GRAFO         | 16- Clasificar Arcos / aCiclicidad    |" << endl;
+    cout << "| 15- DFS                       | 17- Orden Topologico                  |" << endl;
     cout << "o-----------------------------------------------------------------------o" << endl;
 }
 
-void DFS_visit(Grafo &un_grafo, int vertice_actual, int &tiempo, vector<char> &color, vector<int> &tiempo_inicial, vector<int> &tiempo_final){
+void DFS_visit(Grafo &un_grafo, int vertice_actual, int &tiempo, vector<char> &color, vector<int> &tiempo_inicial, vector<int> &tiempo_final, vector<int> &predecesores, vector<datos_dearcos> &listado, stack<int>&topologia){
 
+    datos_dearcos este_arco;
     tiempo++;
     // lo pinta de gris
     color[vertice_actual]= 'G';
     tiempo_inicial[vertice_actual]= tiempo;
-    cout << "vertice " << vertice_actual << " --> GRIS --> tiempo_inicial " << tiempo << endl;
+    //cout << "vertice " << vertice_actual << " --> GRIS --> tiempo_inicial " << tiempo << endl;
 
     // trae vertices adyacentes
     list<Arco> una_lista_arcos;
@@ -65,8 +136,28 @@ void DFS_visit(Grafo &un_grafo, int vertice_actual, int &tiempo, vector<char> &c
     while (un_iterador != una_lista_arcos.end()){
         Arco un_arco= *un_iterador;
         int un_adyacente= un_arco.Devolver_destino();
-        if (color[un_adyacente] == 'B')
-            DFS_visit(un_grafo, un_adyacente, tiempo, color, tiempo_inicial, tiempo_final);
+        este_arco.origen= vertice_actual;
+        este_arco.destino= un_adyacente;
+        
+        // clasifica los arcos y si es blanco lo inspecciona!
+        if (color[un_adyacente] == 'G'){
+            este_arco.tipo= 'B';
+            listado.push_back(este_arco);
+        }
+        if (color[un_adyacente] == 'N'){
+            if (tiempo_inicial[vertice_actual] < tiempo_inicial[un_adyacente]){
+                este_arco.tipo= 'F';
+            }else{
+                este_arco.tipo= 'C';
+            }
+            listado.push_back(este_arco);
+        }
+        if (color[un_adyacente] == 'B'){
+            predecesores[un_adyacente]= vertice_actual;
+            este_arco.tipo= 'T';
+            listado.push_back(este_arco);
+            DFS_visit(un_grafo, un_adyacente, tiempo, color, tiempo_inicial, tiempo_final, predecesores, listado, topologia);
+        }
         un_iterador++;
     }
 
@@ -75,31 +166,38 @@ void DFS_visit(Grafo &un_grafo, int vertice_actual, int &tiempo, vector<char> &c
     tiempo++;
     tiempo_final[vertice_actual]= tiempo;
     color[vertice_actual]= 'N';
-    cout << "vertice " << vertice_actual << " --> NEGRO --> tiempo_final " << tiempo << endl;
-
+    topologia.push(vertice_actual);
 }
 
-void DFS(Grafo &un_grafo){
+void DFS(Grafo &un_grafo, vector<datos_dearcos> &listado, stack<int> &topologia){
     // para todos los vertices: 
     // arma vector colores: B.lanco -> sin visitar, G.ris -> visitado, N.egro-> completo
     // arma vector tiempo visita
     // arma vector tiempo fin
     std::vector<char> color;
+    std::vector<int> predecesores;
     std::vector<int> tiempo_inicial; 
     std::vector<int> tiempo_final;
     int tiempo= 0;
 
+    // vacia los contenedores de las respuestas del dfs (arcos y topologia)
+    listado.clear();
+    
+    while (!topologia.empty())
+        topologia.pop();
+    
+
     // consigue los vertices del grafo:
     list<int> lista_vertices;
     un_grafo.devolver_vertices(lista_vertices);
-    imprimir_lista_vertices(lista_vertices);
-
-    // pone c/vertice en color Blanco
+    
+    // pone c/vertice en color Blanco e inicializa tiempos
     list<int>::iterator iterador= lista_vertices.begin();
     while(iterador != lista_vertices.end()){
         color.push_back('B');
         tiempo_inicial.push_back(-1);
         tiempo_final.push_back(-1);
+        predecesores.push_back(-1);
         iterador++;
     }
 
@@ -108,10 +206,25 @@ void DFS(Grafo &un_grafo){
     while(iterador != lista_vertices.end()){
         int vertice_actual= *iterador;
         if (color[vertice_actual] == 'B')
-            DFS_visit(un_grafo, vertice_actual, tiempo, color, tiempo_inicial, tiempo_final);
+            DFS_visit(un_grafo, vertice_actual, tiempo, color, tiempo_inicial, tiempo_final, predecesores, listado, topologia);
 
         iterador++;
     }
+
+    // imprime el grafo
+    imprimir_grafo(un_grafo);
+
+    // muestra info obtenida
+    cout << "vertice" << "    padre" << "    color" <<"    t. inicial" << "    t.final" << endl;
+    for (int i=0; i<lista_vertices.size(); i++){
+        cout << i << "           " << predecesores[i] << "        " << color[i] <<  "         " << tiempo_inicial[i] <<  "         " << tiempo_final[i] << endl;
+    }
+
+    cout << endl;
+
+
+    
+
 }
 
 
@@ -126,6 +239,12 @@ int main(){
     int vertices, opcion, vertice, origen, destino, costo;
     list<int> listado_vertices;
     list<Arco> lista_arcos;
+                    
+    std::vector<datos_dearcos> listado; // es requerimiento del DFS para aciclicidad
+    stack<int> topologia; // es requerimiento del DFS para Ordenamiento Topologico
+
+
+
     /*
     cout << endl << "Ingrese cantidad de vertices: ";
     cin >> vertices;
@@ -141,8 +260,9 @@ int main(){
     un_grafo.agregar_arco(2, 5, 99);
     un_grafo.agregar_arco(5, 5, 99);
 
-
     cout << endl << "Grafo de " << un_grafo.devolver_longitud() << " vertices creado" << endl << endl;
+
+    //imprimir_grafo(un_grafo);
 
     imprimir_menu();
     cin >> opcion;
@@ -268,7 +388,34 @@ int main(){
             }
                 break;
             case 14:{
-                DFS(un_grafo);
+                imprimir_grafo(un_grafo);
+            }
+                break;
+            case 15:{
+                DFS(un_grafo, listado, topologia);
+            }
+                break;
+            case 16:{
+                DFS(un_grafo, listado, topologia);
+                cout << "origen" << "    destino" << "    tipo de arco" << "     ciclo?" <<endl;
+                for (datos_dearcos i : listado){
+                    cout << i.origen << "          " << i.destino << "          " << i.tipo;     
+                    if (i.tipo == 'B')
+                        cout << "                Si " << endl;
+                    else
+                        cout << "                No " << endl;
+                }
+                
+            }
+                break;
+            case 17:{
+                DFS(un_grafo, listado, topologia);
+                cout << endl << "Ordenamiento Topologico" << endl;
+                while(!topologia.empty()){
+                    cout << topologia.top() << "  ";
+                    topologia.pop();
+                }
+                cout << endl;
             }
                 break;
             default:{
